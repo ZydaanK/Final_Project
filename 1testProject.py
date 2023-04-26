2d0fddf5834be4398c02c4a8c7cec7aaece75a75
Zydaan <zydaan.khan25@bcp.org>
2023-04-26 14:17:19 -0600

    added_players_and_turns

---
+++ 1testProject.py
@@ -0,0 +1,16 @@
+#this file was created by: Zydaan Khan
+
+from tkinter import *
+import random
+
+def new_game():
+
+    global player
+
+    player = random.choice(players)
+
+    label.config(text=player+" turn")
+
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column].config(text="",bg="#F0F0F0")
